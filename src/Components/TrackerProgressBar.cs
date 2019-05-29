d4cec1874778789d408e76b7a0090345948c281b
fragoulin <fabrice.morin@live.com>
2019-05-29 19:03:12 +0200

    Fix progress bar background

--- src/Components/TrackerProgressBar.cs
+++ src/Components/TrackerProgressBar.cs
@@ -5,31 +5,25 @@ namespace PoeRankingTracker.Components
 {
     public partial class TrackerProgressBar : ProgressBar
     {
-        private Color color = Color.Black;
         private Brush brush;
 
         public TrackerProgressBar()
         {
             this.SetStyle(ControlStyles.UserPaint, true);
             this.SetAutoSizeMode(AutoSizeMode.GrowAndShrink);
-            brush = new SolidBrush(color);
         }
 
         public void SetColor(Color color)
         {
-            this.color = color;
             brush = new SolidBrush(color);
         }
 
         protected override void OnPaint(PaintEventArgs e)
         {
             Rectangle rec = e.ClipRectangle;
-
-            rec.Width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
-            if (ProgressBarRenderer.IsSupported)
-                ProgressBarRenderer.DrawHorizontalBar(e.Graphics, e.ClipRectangle);
-            rec.Height -= 4;
-            e.Graphics.FillRectangle(brush, 2, 2, rec.Width, rec.Height);
+            var width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
+            var height = rec.Height - 4;
+            e.Graphics.FillRectangle(brush, 2, 2, width, height);
         }
 
         protected override void Dispose(bool disposing)
@@ -37,7 +31,7 @@ namespace PoeRankingTracker.Components
             if (disposing)
             {
                 components?.Dispose();
-                brush.Dispose();
+                brush?.Dispose();
             }
 
             base.Dispose(disposing);
